3d96e6aeda26645458882958c49214364015bc53
felipinas <jfss2@cin.ufpe.br>
2021-04-29 01:08:04 -0300

    feat(main): improve the fetch code and the search function

--- scripts/index.js
+++ scripts/index.js
@@ -1,46 +1,68 @@
 const pokemonsDiv = document.querySelector(".pokemons");
+const searchInput = document.querySelector("input");
 
 const fetchPokemons = async () => {
 
-    allPokemons = [];
+    const pokemons = Array(150).fill();
 
-    for (id = 1; id <= 150; id++) {
-        const url = `https://pokeapi.co/api/v2/pokemon/${id}`
-        
+    for (id = 1; id <= 151; id++) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+
         try {
-            const pokemon = await fetch(url)
-            const pokemonJson = await pokemon.json()
-            const { id, name , types } = pokemonJson
-            
-            putPokemons(id, name, types)
+            const pokemon = await fetch(url);
+            const pokemonJson = await pokemon.json();
+
+            pokemons[id-1] = pokemonJson;
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
+
+    };
+
+    return pokemons;
+};
+
+const putPokemons = async () => {
+
+    const pokemons = await fetchPokemons();
+
+    pokemons.map(pokemon => {
+
+        let typesDivs = "";
+        const { id, name, types } = pokemon;
+
+        types.map( type => typesDivs += `<div class="${type.type.name}"> <p>${type.type.name}</p> </div>` );
+            
+        pokemonsDiv.innerHTML +=
+            `<div class="pokemon">
+                <img src="https://pokeres.bastionbot.org/images/pokemon/${id}.png" alt="A ${name} image">
+                <span>Nº ${id}</span>
+                <h1>${name}</h1>
+                <div class="types">
+                    ${typesDivs}
+                </div>
+            </div>`;
         
-    };
+    } );
 
-    return allPokemons;
 };
 
-const putPokemons = async (id, name, types) => {
-    console.log(id);
-    let typesDivs = "";
+const searchPokemons = () => {
 
-    for (eachType of types) {
-        typesDivs += `<div class="${eachType.type.name}">
-                    <p>${eachType.type.name}</p>                      
-                    </div>`
-    };
+    const pokemonsDivs = document.querySelectorAll(".pokemon");
     
-    pokemonsDiv.innerHTML +=
-        `<div class="pokemon">
-            <img src="https://pokeres.bastionbot.org/images/pokemon/${id}.png" alt="A ${name} image">
-            <span>Nº ${id}</span>
-            <h1>${name}</h1>
-            <div class="types">
-                ${typesDivs}
-            </div>
-        </div>`;
-};
+    pokemonsDivs.forEach( element => {
+        const pokemonName = element.children[2].innerHTML;
+        const inputValue = searchInput.value.toLowerCase();
+        const inputValueLength = inputValue.length;
 
-fetchPokemons();
+        if ( inputValue == pokemonName.slice(0, inputValueLength) ) {
+            element.style.display = "flex"
+        } else {
+            element.style.display = "none"
+        }
+    });
+}
+
+putPokemons();
+searchInput.addEventListener("input", searchPokemons);
